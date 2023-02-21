f5419dffc6c2fe2ce963373bf04e0e0d853f444b
William Reames <william.reames.19@cnu.edu>
2023-02-20 21:59:07 -0500

    Able to output post from Facebook posts to the console

--- facebook-logger/content.js
+++ facebook-logger/content.js
@@ -1,16 +1,41 @@
 var lastLog = "";
+const startOfPostMarker = " · ";
+const endOfPostMarker = "All reactions:";
 
 window.addEventListener("scroll", () => {
+    // Gather the text on the page
     const newLog = document.getElementById("facebook").innerText;
 
+    // Remove text found previously
     var start = newLog.indexOf(lastLog);
     var end = start + lastLog.length;
     var currentLog = newLog.substring(0, start - 1) + newLog.substring(end);
 
-    if(currentLog.length !== 0){
-        console.log(currentLog);
+    // Limit to only text from posts
+    /* 
+     * Find the index of an "All reactions:". This is the end of a post
+     * Find the last intance of " · " before the location marked as the end of the post. This is the beginning of the post
+     * Take a substring of (start + 3, end). This is the text of the post
+     * Store the post text
+     * Remove the text from the current log
+     * Continue this process until no "All reactions:" can be found in the current log
+     */
+
+    while(currentLog.indexOf(endOfPostMarker) !== -1){
+        var endOfPost = currentLog.indexOf(endOfPostMarker);
+        var textUpToEndOfPost = currentLog.substring(0, endOfPost);
+        var startOfPost = textUpToEndOfPost.lastIndexOf(startOfPostMarker);
+
+        var facebookPostText = textUpToEndOfPost.substring(startOfPost + startOfPostMarker.length);
+        if(facebookPostText.length !== 0){
+            console.log(facebookPostText);
+        }
+
+        currentLog = currentLog.replace(startOfPostMarker + facebookPostText + endOfPostMarker, "");
     }
+
     if(newLog.length !== 0){
-        lastLog = newLog;
+        var lastEndOfPost = newLog.lastIndexOf(endOfPostMarker);
+        lastLog = newLog.substring(0, lastEndOfPost + endOfPostMarker.length);
     }
 });
